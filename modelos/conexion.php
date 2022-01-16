e3f22947b470fc9cf7a80aa42a7db8fc02c0366a
mikelson.laince <mikelson.laince@gmail.com>
2022-01-16 13:24:03 -0300

    ultimos cambios db

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -8,7 +8,7 @@ class Conexion{
 		$cleardb_server = $cleardb_url["us-cdbr-east-05.cleardb.net"];
 		$cleardb_username = $cleardb_url["b9b43eb54db2c4"];
 		$cleardb_password = $cleardb_url["b0aeba46"];
-		$cleardb_db = substr($cleardb_url["path"],1);
+		$cleardb_db = substr($cleardb_url["heroku_ed0c3f3800533e3"],1);
 		$active_group = 'default';
 		$query_builder = TRUE;
 		// Connect to DB
