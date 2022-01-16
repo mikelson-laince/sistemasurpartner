85ad37e7f20b684d331a1b535f55e164a5229aaa
mikelson.laince <mikelson.laince@gmail.com>
2022-01-16 12:22:20 -0300

    actualizacion del archivo de conexion

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -11,3 +11,14 @@ class Conexion{
 		return $link;
 	}
 }
+
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["us-cdbr-east-05.cleardb.net"];
+$cleardb_username = $cleardb_url["b9b43eb54db2c4"];
+$cleardb_password = $cleardb_url["b0aeba46"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
