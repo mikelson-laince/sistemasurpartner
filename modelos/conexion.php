b0c97373400d9d811d508a55d4082107d312d804
mikelson.laince <mikelson.laince@gmail.com>
2022-01-16 12:59:20 -0300

    ultimos cambios en conexion.php

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,7 +4,7 @@ class Conexion{
 
 	public function conectar(){
 		//Get Heroku ClearDB connection information
-		$cleardb_url = parse_url(getenv("heroku_ed0c3f3800533e3"));
+		$cleardb_url = parse_url(getenv("mysql://b9b43eb54db2c4:b0aeba46@us-cdbr-east-05.cleardb.net/heroku_ed0c3f3800533e3?reconnect=true"));
 		$cleardb_server = $cleardb_url["us-cdbr-east-05.cleardb.net"];
 		$cleardb_username = $cleardb_url["b9b43eb54db2c4"];
 		$cleardb_password = $cleardb_url["b0aeba46"];
