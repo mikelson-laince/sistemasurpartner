47ddb8ceb61cd461c30faafbba887a364f3883a9
mikelson.laince <mikelson.laince@gmail.com>
2022-01-16 01:18:05 -0300

    actualizacion de datos

--- index.php
+++ index.php
@@ -14,9 +14,9 @@ $plantilla -> ctrPlantilla();
 
 //Get Heroku ClearDB connection information
 $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
+$cleardb_server = $cleardb_url["us-cdbr-east-05.cleardb.net"];
+$cleardb_username = $cleardb_url["b9b43eb54db2c4"];
+$cleardb_password = $cleardb_url["b0aeba46"];
 $cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
